e814ae36c066eb3649fad918263db433f4bc514a
Lucavinini <lucas.00000850855@unicap.br>
2026-01-18 14:00:04 -0300

    feat: corrige vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/lacbag-industrial-solutions/",
   plugins: [react()],
   server: {
     port: 3000,
