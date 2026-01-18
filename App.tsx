7ee548f0c2660db7a4190d6eec2bd1087fbd3e86
Lucavinini <lucas.00000850855@unicap.br>
2026-01-18 13:56:25 -0300

    feat: sobe projeto

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
-
-import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Products from './components/Products';
-import Services from './components/Services';
-import About from './components/About';
-import Location from './components/Location';
-import Footer from './components/Footer';
-import QuoteAssistant from './components/QuoteAssistant';
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Products from "./components/Products";
+import Services from "./components/Services";
+import About from "./components/About";
+import Location from "./components/Location";
+import Footer from "./components/Footer";
 
 const App: React.FC = () => {
   return (
@@ -20,9 +18,6 @@ const App: React.FC = () => {
         <About />
         <Location />
       </main>
-      <Footer />
-      {/* AI Assistant to help customers find the right product */}
-      <QuoteAssistant />
     </div>
   );
 };
